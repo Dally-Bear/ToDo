e07215a53c7cd7719c974602c8c34d861903d512
Dallybearr <Dallasmaxim15@icloud.com>
2024-10-11 09:08:03 -0600

    ToDo lab 2

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import ToDoList from './components/ToDoList';
+import ToDoForm from './components/ToDoForm';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <main>
+      <ToDoList />
+      <ToDoForm />
+    </main>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
